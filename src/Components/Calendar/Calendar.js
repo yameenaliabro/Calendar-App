b3a8f7361377c3ea0b3182eacb903e268ca071b4
yameenaliabro <muhammadyameen5432@gmail.com>
2023-05-05 17:12:31 +0500

    some width change

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -9,12 +9,10 @@ function Calendar() {
   const [today,settoday] = useState(new Date())
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const firstDay = new Date(year, month, 1).getDay();
-
   const handlePrevMonth = () => {
     setMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
     setYear((prevYear) => (month === 0 ? prevYear - 1 : prevYear));
   };
-
   const handleNextMonth = () => {
     setMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1));
     setYear((prevYear) => (month === 11 ? prevYear + 1 : prevYear));
@@ -24,8 +22,8 @@ function Calendar() {
       <div className='main'>
         <h1>Calendar App</h1>
         <div className="header">
-          <Button onClick={handlePrevMonth} type="primary">{"<"}</Button>
-          <h2 >{new Date(year, month).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}</h2>
+          <Button onClick={handlePrevMonth} type="primary">{"<"}</Button><br/>
+          <h2>{new Date(year, month).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}</h2><br/>
           <Button onClick={handleNextMonth} type="primary">{">"}</Button>
         </div>
         <div className="days">
